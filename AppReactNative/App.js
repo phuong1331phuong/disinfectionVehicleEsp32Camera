b87770e1871df0c933ea81783e05218c753718ff
phuong1331phuong <85828804+phuong1331phuong@users.noreply.github.com>
2022-01-12 13:18:47 +0700

    commit init

---
+++ AppReactNative/App.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+
+export default function App() {
+	return (
+		<SafeAreaView>
+			<Text>Hello, World!</Text>
+		</SafeAreaView>
+	);
+}
---
+++ ArduimoEsp32Camera/ESP32CAM_USPSerial.ino
@@ -0,0 +1,144 @@
+#include "esp_camera.h"
+#include <base64.hpp>
+//#include "camera_pins.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define CAMERA_MODEL_AI_THINKER
+
+const char *ssid = "Trầm cảm lên";
+const char *password = "Matkhaulatenwifichumviethoa";
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "esp32/test";
+const char *mqtt_username = "phuongbgbg";
+const char *mqtt_password = "1234567";
+const int mqtt_port = 1883;
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+const int bufferSize = 1024 * 23;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void cameraInit()
+{
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.frame_size = FRAMESIZE_VGA; // 640x480
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        ESP.restart();
+        return;
+    }
+}
+
+
+//void grabImage(){
+//  camera_fb_t * fb = esp_camera_fb_get();
+//  if(fb != NULL && fb->format == PIXFORMAT_JPEG && fb->len < bufferSize){
+//    Serial.print("Image Length: ");
+//    Serial.print(fb->len);
+//    Serial.print("\t Publish Image: ");
+////    bool result = client.publish(ESP32CAM_PUBLISH_TOPIC, (const char*)fb->buf, fb->len);
+//    Serial.println(result);
+//
+//    if(!result){
+//      ESP.restart();
+//    }
+//  }
+//  esp_camera_fb_return(fb);
+//  delay(1);
+//}
+
+void setup()
+{
+    Serial.begin(115200);
+    cameraInit();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected())
+    {
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+        {
+            Serial.println("Public emqx mqtt broker connected");
+        }
+        else
+        {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+    // publish and subscribe
+    client.publish(topic, "Hi EMQ X I'm ESP32 ^^");
+    client.subscribe(topic);
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+void loop()
+{
+    client.loop();
+//    if (client.connected())
+//        grabImage();
+}
